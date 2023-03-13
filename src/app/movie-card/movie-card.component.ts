62918539e339d1579efd04400857d881ec6d6b3d
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2023-03-13 21:43:28 +0500

    add movie to local storage

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { MovieItem } from '../movie-list/movie-list.component';
 import { GetGenreService } from '../service/get-genre.service';
+import { LocalStorageService } from '../service/local-storage.service';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,9 +10,17 @@ import { GetGenreService } from '../service/get-genre.service';
   styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent {
-  constructor(private genreService: GetGenreService) {}
+  constructor(
+    private genreService: GetGenreService,
+    private localStorageService: LocalStorageService
+  ) {}
   @Input() movie!: MovieItem;
   @Input() isHover!: boolean;
+
+  setBestMovie() {
+    const movieJSON = JSON.stringify(this.movie);
+    this.localStorageService.saveData(String(this.movie.id), movieJSON);
+  }
 
   getGenre(genreNumberList: number[]) {
     return this.genreService.getGenreNameList(genreNumberList);
